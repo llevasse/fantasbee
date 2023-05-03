030b3e020a77e0dd343eaa71c5a0fdd607aabed1
Leo <leonardlevasseur@yahoo.com>
2023-05-03 18:38:02 +0200

    fix texture

--- src/main/java/net/llevasse/fantasbee/block/custom/suspecious_beehive_block.java
+++ src/main/java/net/llevasse/fantasbee/block/custom/suspecious_beehive_block.java
@@ -7,7 +7,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
